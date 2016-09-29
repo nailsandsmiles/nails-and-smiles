5deb18a27d22c828f54b9cb5c657db98ec551295
jaga <jagawebdev@gmail.com>
2016-09-29 04:33:29 +0000

    add anguler

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*global $*/
+
+$(document).ready(function() {
+  
+  $('.hamburger').click( function(event){
+        event.stopPropagation();
+        $('.drop-menu').toggle();
+    });
+
+    $(document).click( function(){
+        $('.drop-menu').hide();
+    });
+
+});
