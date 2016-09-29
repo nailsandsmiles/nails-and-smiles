943278976da3001a0a21fbbbbc6f6413e290200c
jaga <jagawebdev@gmail.com>
2016-09-29 04:56:19 +0000

    anguler templates

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*global angular*/
+
+var app = angular.module("nailsAndSmiles", ["ngRoute"])
+                .config(["$routeProvider", function ($routeProvider) {
+                  $routeProvider
+                    .when("/landing",
+                    {
+                        templateUrl: "templates/landing.html"
+                    })
+                    .when("/services",
+                    {
+                        templateUrl: "templates/services.html"
+                    })
+                    .when("/gallery",
+                    {
+                        templateUrl: "templates/gallery.html"
+                    })
+                    .when("/hours",
+                    {
+                        templateUrl: "templates/hours.html"
+                    })
+                    .when("/contact",
+                    {
+                        templateUrl: "templates/contact.html"
+                    })
+                    .otherwise({redirectTo: "/landing"});
+}]);
